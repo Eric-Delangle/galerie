2edeac267d750dd8a0975fb0123aaeec48a6f33a
eric <polvu@hotmail.fr>
2021-04-25 19:43:42 +0200

    installation de swiftMailer

---
+++ src/Controller/EmailController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use Swift;
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SwiftmailerBundle\Command\SendEmailCommand;
+
+class EmailController extends AbstractController
+{
+
+    /**
+     * @var \Swift Mailer
+     */
+    private $mailer;
+
+
+    public function __construct(\Swift_Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+
+    /**
+     * @Route("/email", name="contact_email")
+     */
+    public function contact(Request $request)
+    {
+
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactType::class, $contact);
+
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $message = (new \Swift_Message($contact))
+                ->setFrom('info@ericdelangle-deco.fr')
+                ->setTo('polvu@hotmail.fr')
+                ->setSubject($contact->getSubject())
+                ->setReplyTo($contact->getEmail())
+                ->setBody($contact->getMessage(), 'text/html');
+
+            $this->addFlash('success', 'Votre message a bien été envoyé !');
+
+            dump($this->mailer, $message);
+
+            $this->mailer->send($message);
+
+
+            return $this->redirectToRoute('member_account');
+        }
+
+        return $this->render('email/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
